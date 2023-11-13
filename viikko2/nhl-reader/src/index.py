7812904df3140bbad26ecc5f3a03c0462c55586a
ds20220914 <davidkuul2002@gmail.com>
2023-11-13 18:28:07 +0200

    tehtävä12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,14 @@
 import requests
 from player import Player
-
+from player import PlayerReader
+from player import PlayerStats
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+   url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+   reader = PlayerReader(url)
+   stats = PlayerStats(reader)
+   players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-    finnish_players = filter(lambda player: player.nationality == "FIN", players)
-    sorted_players = sorted(finnish_players, key=lambda p: p.goals + p.assists, reverse=True)
-
-
-    print("Players from FIN\n")
-    for player in sorted_players:
-        print(player)
-
+   for player in players:
+       print(player)
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
@@ -10,3 +13,21 @@ class Player:
         goals_assists = self.goals + self.assists
         return f"{self.name:20} {self.team:4}  {self.goals:2}+{self.assists:2}={goals_assists:3}"
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        return [Player(player_data) for player_data in response]
+
+class PlayerStats:
+    def __init__(self, reader: PlayerReader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality: str):
+        players = self.reader.get_players()
+        finnish_players = filter(lambda player: player.nationality == nationality, players)
+        sorted_players = sorted(finnish_players, key=lambda p: p.goals + p.assists, reverse=True)
+
+        return sorted_players
